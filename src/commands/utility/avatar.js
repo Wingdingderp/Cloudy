9878023d34a98fd4474de667dfbb7f4bd268f896
General Kenobi <GeneralKenobi2485@gmail.com>
2024-05-02 00:50:39 -0400

    got a command to get a user's avatar working

---
+++ src/commands/utility/avatar.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Get a user\'s avatar')
+        .addUserOption((option) => //creates new slash command option
+        option.setName('user')
+        .setDescription('The user who\'s avatar you want')
+        .setRequired(false)
+    ),
+
+    async execute(interaction) {
+        const { channel, client, options, member } = interaction;
+        let user = interaction.options.getUser('user') || interaction.member; //Sets user to the response to user option or to the member running command
+        let userAvatar = user.displayAvatarURL({ size: 512 }); //Displays user avatar with dimensions 512x512
+
+        const emebd = new EmbedBuilder()
+        .setColor(0x0000FF)
+        .setTitle(`${user}'s Avatar`)
+        .setImage(`${userAvatar}`);
+
+        const button = new ButtonBuilder()
+        .setLabel('Avatar Link')
+        .setStyle(ButtonStyle.Link)
+        .setURL(`${user.avatarURL({ size: 512 })}`);
+        
+        const row = new ActionRowBuilder().addComponents(button);
+
+        await interaction.reply({
+            embeds: [embed],
+            components: [row],
+        });
+    },
+};
