26549bbe63e0353c39d323097fc4b2a4a01ac4bf
Wingdingderp <shelbykaiden@gmail.com>
2024-05-02 21:43:35 -0400

    Make Ping reply back silently

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply({content:'Pong!', ephemeral:true});
 	},
+	ephemeral: true
 };
