b98c0ada0b0552d5498ff0dbf8350628bcea5489
Wingdingderp <shelbykaiden@gmail.com>
2024-05-08 20:12:34 -0400

    Some more moderation commands x2

---
+++ src/commands/moderation/lock.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require(`@discordjs/builders`);
+const { EmbedBuilder, PermissionsBitField, ChannelType } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('lock')
+    .setDescription(`Locks a given channel`)
+    .addChannelOption(option => option.setName('channel').setDescription(`The channel you want to lock`).addChannelTypes(ChannelType.GuildText).setRequired(true)),
+    async execute (interaction) {
+
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) return await interaction.reply({ content: "You don't have permissions to execute this command!", ephemeral: true})
+
+        let channel = interaction.options.getChannel('channel');
+
+        channel.permissionOverwrites.create(interaction.guild.id, { SendMessages: false})
+
+        const embed = new EmbedBuilder()
+        .setColor("Blue")
+        .setDescription(`:white_check_mark:  ${channel} has been **locked**`)
+
+        await interaction.reply({ embeds: [embed] })
+    }
+}
